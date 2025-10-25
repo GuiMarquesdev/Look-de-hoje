b85a04165fede193642f96b7eb753a2f86a9d12e
Guilherme <guimarquesbrito@gmail.com>
2025-10-25 15:52:50 -0300

    erro de propriedade não existente pendente

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// C:\Users\Guilherme\Desktop\Look-de-hoje\frontend\src\main.tsx
 
-createRoot(document.getElementById("root")!).render(<App />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx"; // Certifique-se que o caminho para App.tsx está correto
+import "./index.css"; // Importa o CSS global
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
